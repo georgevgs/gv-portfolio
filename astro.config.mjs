fdbed4f11fc68899b4d48f58dedf25f9f94a57c7
George Vagdas <vagdas@pm.me>
2023-01-21 21:12:29 +0200

    remove meta tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-<meta
-  http-equiv="Content-Security-Policy"
-  content="default-src 'self'; img-src https://*; child-src 'none';" />
-
 // https://astro.build/config
 export default defineConfig({});
