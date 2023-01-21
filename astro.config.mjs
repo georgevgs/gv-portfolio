645d0b73877a3a5d2eb382c24796fa786b72945e
George Vagdas <vagdas@pm.me>
2023-01-21 20:39:54 +0200

    added CSP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+<meta
+  http-equiv="Content-Security-Policy"
+  content="default-src 'self'; img-src https://*; child-src 'none';" />
+
 // https://astro.build/config
 export default defineConfig({});
