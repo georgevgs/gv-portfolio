3eb87d7f5fad4672d5ab1ebf99e6c605f38c101f
George Vagdas <dev@vagdas.eu>
2024-05-24 00:12:52 +0300

    Added TKS bento box

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'linkedin-blue': '#0072b1',
-				'github-black': '#24292e',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "linkedin-blue": "#0072b1",
+        "github-black": "#24292e",
+        "neon-orange": "#ff5f1f",
+      },
+    },
+  },
+  plugins: [],
+};
